52daa85a2aae6ce72fcd351b7380a827a0dac4f7
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2021-04-05 15:41:14 -0400

    improve(deployment): Add deploy.sh script (#4)

---
+++ packages/financial-contracts/scripts/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NETWORK=$1
+SUBGRAPH_NAME=$NETWORK'-contracts'
+if [ "$STAGING" ]; then
+  SUBGRAPH_NAME=$SUBGRAPH_NAME-staging
+fi
+if [ "$STAGING" ]; then
+  NAMESPACE="nicholaspai"
+else 
+  NAMESPACE="umaprotocol"
+fi
+
+echo 'Loading graph access token from .env'
+export $(cat .env)
+API_KEY=$PROD_KEY
+
+# Require $GRAPHKEY to be set
+if [[ -z "${API_KEY}" ]]; then
+>&2 echo "PROD_KEY not defined in .env. This should be the access token for the $NAMESPACE graph namespace"
+exit 1
+fi
+
+graph deploy --node https://api.thegraph.com/deploy/ --ipfs https://api.thegraph.com/ipfs/ $NAMESPACE/$SUBGRAPH_NAME --access-token $API_KEY
