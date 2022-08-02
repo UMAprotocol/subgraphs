c9f70d82de12ba8de9c5caa099f2c1978d61b9db
Pablo Maldonado <pablo@umaproject.org>
2022-08-02 11:13:00 +0100

    feat: add remaining preexisting handlers
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/votingV2/src/mappings/votingV2.ts
+++ packages/votingV2/src/mappings/votingV2.ts
@@ -104,73 +104,6 @@ export function handlePriceResolved(event: PriceResolved): void {
   voterGroup.save();
 }
 
-// - event: RewardsRetrieved(indexed address,indexed uint256,indexed bytes32,uint256,uint256)
-//   handler: handleRewardsRetrieved
-//  event RewardsRetrieved(
-//      address indexed voter,
-//      uint256 indexed roundId,
-//      bytes32 indexed identifier,
-//      uint256 time,
-//      uint256 numTokens
-//  );
-
-// export function handleRewardsRetrieved(event: RewardsRetrieved): void {
-//   let rewardClaimedId = event.params.voter
-//     .toHexString()
-//     .concat("-")
-//     .concat(event.params.identifier.toString())
-//     .concat("-")
-//     .concat(event.params.time.toString())
-//     .concat("-")
-//     .concat(event.params.roundId.toString());
-//   let rewardClaimed = getOrCreateRewardsClaimed(rewardClaimedId);
-//   let claimer = getOrCreateUser(event.params.voter);
-//   let requestId = event.params.identifier.toString().concat("-").concat(event.params.time.toString());
-//   let requestRound = getOrCreatePriceRequestRound(requestId.concat("-").concat(event.params.roundId.toString()));
-//   let winnerGroup: VoterGroup | null =
-//     requestRound.winnerGroup != null ? getOrCreateVoterGroup(requestRound.winnerGroup) : null;
-
-//   rewardClaimed.claimer = claimer.id;
-//   rewardClaimed.round = requestRound.id;
-//   rewardClaimed.request = requestId;
-//   rewardClaimed.identifier = event.params.identifier.toString();
-//   rewardClaimed.time = event.params.time;
-//   rewardClaimed.numTokens = event.params.numTokens;
-
-//   // If rewards > 0, then voter correctly voted for this round.
-//   if (event.params.numTokens > BIGINT_ZERO) {
-//     claimer.countRetrievals = claimer.countRetrievals + BIGINT_ONE;
-//   }
-
-//   requestRound.request = requestId;
-//   requestRound.identifier = event.params.identifier.toString();
-//   requestRound.time = event.params.time;
-//   requestRound.roundId = event.params.roundId;
-//   requestRound.totalRewardsClaimed = requestRound.totalRewardsClaimed + toDecimal(rewardClaimed.numTokens);
-//   if (
-//     rewardClaimed.numTokens > BIGINT_ZERO &&
-//     winnerGroup.votersAmount > BIGDECIMAL_ZERO &&
-//     requestRound.totalSupplyAtSnapshot > BIGDECIMAL_ZERO
-//   ) {
-//     requestRound.votersClaimedAmount = requestRound.votersClaimedAmount + BIGDECIMAL_ONE;
-//     requestRound.votersClaimedRatio =
-//       winnerGroup != null && winnerGroup.votersAmount != BIGDECIMAL_ZERO
-//         ? requestRound.votersClaimedAmount / winnerGroup.votersAmount
-//         : requestRound.votersClaimedRatio;
-//     requestRound.votersClaimedPercentage = requestRound.votersClaimedRatio * BIGDECIMAL_HUNDRED;
-//     requestRound.tokensClaimedRatio =
-//       requestRound.inflationRateRaw != null && requestRound.inflationRateRaw != BIGDECIMAL_ZERO
-//         ? requestRound.totalRewardsClaimed /
-//           (requestRound.totalSupplyAtSnapshot * <BigDecimal>requestRound.inflationRateRaw)
-//         : requestRound.tokensClaimedRatio;
-//     requestRound.tokensClaimedPercentage = requestRound.tokensClaimedRatio * BIGDECIMAL_HUNDRED;
-//   }
-
-//   requestRound.save();
-//   rewardClaimed.save();
-//   claimer.save();
-// }
-
 // - event: VoteCommitted(indexed address,indexed uint256,indexed bytes32,uint256)
 //   handler: handleVoteCommitted
 //  event VoteCommitted(address indexed voter, uint256 indexed roundId, bytes32 indexed identifier, uint256 time);
@@ -216,59 +149,59 @@ export function handleVoteCommitted(event: VoteCommitted): void {
 //      uint256 numTokens
 //  );
 
-// export function handleVoteRevealed(event: VoteRevealed): void {
-//   let voteId = event.params.voter
-//     .toHexString()
-//     .concat("-")
-//     .concat(event.params.identifier.toString())
-//     .concat("-")
-//     .concat(event.params.time.toString())
-//     .concat("-")
-//     .concat(event.params.roundId.toString());
-//   let vote = getOrCreateRevealedVote(voteId);
-//   let voter = getOrCreateUser(event.params.voter);
-//   let requestId = event.params.identifier.toString().concat("-").concat(event.params.time.toString());
-//   let requestRound = getOrCreatePriceRequestRound(requestId.concat("-").concat(event.params.roundId.toString()));
-//   let groupId = requestRound.id.concat("-").concat(event.params.price.toString());
-//   let voterGroup = getOrCreateVoterGroup(groupId);
-//   let votingContract = VotingV2.bind(event.address);
-//   let roundInfo = votingContract.try_rounds(event.params.roundId);
+export function handleVoteRevealed(event: VoteRevealed): void {
+  let voteId = event.params.voter
+    .toHexString()
+    .concat("-")
+    .concat(event.params.identifier.toString())
+    .concat("-")
+    .concat(event.params.time.toString())
+    .concat("-")
+    .concat(event.params.roundId.toString());
+  let vote = getOrCreateRevealedVote(voteId);
+  let voter = getOrCreateUser(event.params.voter);
+  let requestId = event.params.identifier.toString().concat("-").concat(event.params.time.toString());
+  let requestRound = getOrCreatePriceRequestRound(requestId.concat("-").concat(event.params.roundId.toString()));
+  let groupId = requestRound.id.concat("-").concat(event.params.price.toString());
+  let voterGroup = getOrCreateVoterGroup(groupId);
+  let votingContract = VotingV2.bind(event.address);
+  let roundInfo = votingContract.try_rounds(event.params.roundId);
 
-//   vote.voter = voter.id;
-//   vote.round = requestRound.id;
-//   vote.request = requestId;
-//   vote.identifier = event.params.identifier.toString();
-//   vote.time = event.params.time;
-//   vote.price = event.params.price;
-//   vote.numTokens = event.params.numTokens;
-//   vote.group = voterGroup.id;
+  vote.voter = voter.id;
+  vote.round = requestRound.id;
+  vote.request = requestId;
+  vote.identifier = event.params.identifier.toString();
+  vote.time = event.params.time;
+  vote.price = event.params.price;
+  vote.numTokens = event.params.numTokens;
+  vote.group = voterGroup.id;
 
-//   voter.countReveals = voter.countReveals + BIGINT_ONE;
+  voter.countReveals = voter.countReveals.plus(BIGINT_ONE);
 
-//   voterGroup.price = event.params.price;
-//   voterGroup.round = requestRound.id;
-//   voterGroup.totalVoteAmount = voterGroup.totalVoteAmount + toDecimal(vote.numTokens);
-//   voterGroup.votersAmount = voterGroup.votersAmount + BIGDECIMAL_ONE;
+  voterGroup.price = event.params.price;
+  voterGroup.round = requestRound.id;
+  voterGroup.totalVoteAmount = voterGroup.totalVoteAmount.plus(toDecimal(vote.numTokens));
+  voterGroup.votersAmount = voterGroup.votersAmount.plus(BIGDECIMAL_ONE);
 
-//   requestRound.request = requestId;
-//   requestRound.identifier = event.params.identifier.toString();
-//   requestRound.time = event.params.time;
-//   requestRound.roundId = event.params.roundId;
-//   requestRound.totalVotesRevealed = requestRound.totalVotesRevealed + toDecimal(vote.numTokens);
-//   requestRound.votersAmount = requestRound.votersAmount + BIGDECIMAL_ONE;
-//   requestRound.snapshotId = roundInfo.reverted ? null : roundInfo.value.value0;
-//   if (requestRound.snapshotId != null && requestRound.totalSupplyAtSnapshot == null) {
-//     let supply = getTokenContract().try_totalSupplyAt(<BigInt>requestRound.snapshotId);
-//     requestRound.totalSupplyAtSnapshot = supply.reverted ? null : toDecimal(supply.value as BigInt);
-//   }
-//   requestRound.tokenVoteParticipationRatio =
-//     requestRound.totalSupplyAtSnapshot != null
-//       ? requestRound.totalVotesRevealed / <BigDecimal>requestRound.totalSupplyAtSnapshot
-//       : null;
-//   requestRound.tokenVoteParticipationPercentage = requestRound.tokenVoteParticipationRatio * BIGDECIMAL_HUNDRED;
+  requestRound.request = requestId;
+  requestRound.identifier = event.params.identifier.toString();
+  requestRound.time = event.params.time;
+  requestRound.roundId = event.params.roundId;
+  requestRound.totalVotesRevealed = requestRound.totalVotesRevealed.plus(toDecimal(vote.numTokens));
+  requestRound.votersAmount = requestRound.votersAmount.plus(BIGDECIMAL_ONE);
+  requestRound.snapshotId = roundInfo.reverted ? null : roundInfo.value.value0;
+  if (requestRound.snapshotId != null && requestRound.totalSupplyAtSnapshot == null) {
+    let supply = getTokenContract().try_totalSupplyAt(<BigInt>requestRound.snapshotId);
+    requestRound.totalSupplyAtSnapshot = supply.reverted ? null : toDecimal(supply.value as BigInt);
+  }
+  requestRound.tokenVoteParticipationRatio =
+    requestRound.totalSupplyAtSnapshot != null
+      ? requestRound.totalVotesRevealed.div(requestRound.totalSupplyAtSnapshot)
+      : null;
+  requestRound.tokenVoteParticipationPercentage = requestRound.tokenVoteParticipationRatio.times(BIGDECIMAL_HUNDRED);
 
-//   requestRound.save();
-//   vote.save();
-//   voter.save();
-//   voterGroup.save();
-// }
+  requestRound.save();
+  vote.save();
+  voter.save();
+  voterGroup.save();
+}
