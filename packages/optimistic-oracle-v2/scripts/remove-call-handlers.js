c58e61499e76c9bab29fcaf09360521cfc6f42f9
ryanwolhuter <dev@ryanwolhuter.com>
2023-02-20 15:43:33 +0200

    simplify
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/optimistic-oracle-v2/scripts/remove-call-handlers.js
+++ packages/optimistic-oracle-v2/scripts/remove-call-handlers.js
@@ -1,14 +1,8 @@
-const fs = require("fs")
+const fs = require("fs");
 const yaml = require("yaml");
 
-function main() {
-  const subgraphYaml = fs.readFileSync('subgraph.yaml', 'utf8')
-  const parsed = yaml.parse(subgraphYaml);
-  delete parsed.dataSources[0].mapping.callHandlers;
-  const stringified = yaml.stringify(parsed);
-  console.log(stringified);
-  // write the parsed data back to the file
-  fs.writeFileSync('subgraph.yaml', stringified);
-}
-
-main();
+const subgraphYaml = fs.readFileSync("subgraph.yaml", "utf8");
+const parsed = yaml.parse(subgraphYaml);
+delete parsed.dataSources[0].mapping.callHandlers;
+const stringified = yaml.stringify(parsed);
+fs.writeFileSync("subgraph.yaml", stringified);
