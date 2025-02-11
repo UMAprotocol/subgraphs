634fa07c37f884c3daffec3638f1a05d185f184d
Pablo Maldonado <pablo@umaproject.org>
2025-02-11 16:21:12 +0100

    feat: rename env var
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/optimistic-oracle-v2/scripts/build-manifest.sh
+++ packages/optimistic-oracle-v2/scripts/build-manifest.sh
@@ -6,10 +6,10 @@ DATA=manifest/data/$FILE
 
 echo "Generating manifest from data file: $DATA"
 
-# If RENAME_CHAIN_ID is set, update the network field in the JSON data on the fly
-if [[ -n "$RENAME_CHAIN_ID" ]]; then
-  echo "Updating network field to: $RENAME_CHAIN_ID"
-  UPDATED_DATA=$(jq --arg newNetwork "$RENAME_CHAIN_ID" '.network = $newNetwork' "$DATA")
+# If OVERRIDE_CHAIN_ID is set, update the network field in the JSON data on the fly
+if [[ -n "$OVERRIDE_CHAIN_ID" ]]; then
+  echo "Updating network field to: $OVERRIDE_CHAIN_ID"
+  UPDATED_DATA=$(jq --arg newNetwork "$OVERRIDE_CHAIN_ID" '.network = $newNetwork' "$DATA")
 else
   UPDATED_DATA=$(cat "$DATA")
 fi
