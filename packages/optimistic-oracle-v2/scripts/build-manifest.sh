184694ef6e0036b01f36ba4ace4ff63bae59cfba
Pablo Maldonado <pablo@umaproject.org>
2025-02-11 12:58:24 +0100

    feat: fix goldsky deployments
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/optimistic-oracle-v2/scripts/build-manifest.sh
+++ packages/optimistic-oracle-v2/scripts/build-manifest.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
 NETWORK=$1
-
 FILE=$NETWORK'.json'
-
 DATA=manifest/data/$FILE
 
-echo 'Generating manifest from data file: '$DATA
-cat $DATA
+echo "Generating manifest from data file: $DATA"
 
+# If RENAME_CHAIN_ID is set, update the network field in the JSON data on the fly
+if [[ -n "$RENAME_CHAIN_ID" ]]; then
+  echo "Updating network field to: $RENAME_CHAIN_ID"
+  UPDATED_DATA=$(jq --arg newNetwork "$RENAME_CHAIN_ID" '.network = $newNetwork' "$DATA")
+else
+  UPDATED_DATA=$(cat "$DATA")
+fi
+
+# Output the modified JSON data for debugging
+echo "$UPDATED_DATA"
+
+# Create a temporary file to store the modified JSON data
+TEMP_JSON=$(mktemp)
+echo "$UPDATED_DATA" > "$TEMP_JSON"
+
+# Generate subgraph.yaml using mustache with the modified JSON data
 mustache \
   -p manifest/templates/OptimisticOracleV2.template.yaml \
-  $DATA \
+  "$TEMP_JSON" \
   manifest/templates/subgraph.template.yaml > subgraph.yaml
+
+# Clean up the temporary file
+rm "$TEMP_JSON"
