190f0dadd502b079db782747b00b00e9965a676f
Gerhard Steenkamp <gerhard@umaproject.org>
2025-09-11 18:18:44 +0200

    update currency for multiple overriding custom bonds
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- packages/managed-oracle-v2/src/mappings/managedOracleV2.ts
+++ packages/managed-oracle-v2/src/mappings/managedOracleV2.ts
@@ -13,9 +13,9 @@ export function handleCustomBondSet(event: CustomBondSet): void {
     entity.requester = event.params.requester;
     entity.identifier = event.params.identifier.toString();
     entity.ancillaryData = event.params.ancillaryData.toHex();
-    entity.currency = event.params.currency;
   }
 
+  entity.currency = event.params.currency;
   entity.customBond = event.params.bond;
   entity.save();
 }
