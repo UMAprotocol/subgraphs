793602451a177da5b5624c36310b43946191c656
Gerhard Steenkamp <gerhard@umaproject.org>
2025-09-18 13:38:46 +0200

    derive custom bond key from currency
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- packages/managed-oracle-v2/src/mappings/managedOracleV2.ts
+++ packages/managed-oracle-v2/src/mappings/managedOracleV2.ts
@@ -1,15 +1,19 @@
 import { log } from "@graphprotocol/graph-ts";
 import { CustomBond, CustomLiveness } from "../../generated/schema";
 import { CustomBondSet, CustomLivenessSet } from "../../generated/ManagedOracleV2/ManagedOracleV2";
+import { createCustomBondIdFromEvent } from "../utils/helpers/managedOracleV2";
 
 export function handleCustomBondSet(event: CustomBondSet): void {
   const managedRequestId = event.params.managedRequestId.toHexString();
   log.debug("Custom Bond set event. Loading entity with managedRequestId, {}", [managedRequestId]);
 
-  let entity = CustomBond.load(managedRequestId);
+  const id = createCustomBondIdFromEvent(event);
+
+  let entity = CustomBond.load(id);
 
   if (entity == null) {
     entity = new CustomBond(managedRequestId);
+    entity.managedRequestId = managedRequestId;
     entity.requester = event.params.requester;
     entity.identifier = event.params.identifier.toString();
     entity.ancillaryData = event.params.ancillaryData.toHex();
@@ -28,6 +32,7 @@ export function handleCustomLivenessSet(event: CustomLivenessSet): void {
 
   if (entity == null) {
     entity = new CustomLiveness(managedRequestId);
+    entity.managedRequestId = managedRequestId;
     entity.requester = event.params.requester;
     entity.identifier = event.params.identifier.toString();
     entity.ancillaryData = event.params.ancillaryData.toHex();
--- packages/managed-oracle-v2/src/utils/helpers/managedOracleV2.ts
+++ packages/managed-oracle-v2/src/utils/helpers/managedOracleV2.ts
@@ -1,6 +1,21 @@
 import { Address, ByteArray, Bytes, crypto } from "@graphprotocol/graph-ts";
+import { CustomBondSet } from "../../../generated/ManagedOracleV2/ManagedOracleV2";
 
 export function getManagedRequestId(requester: Address, identifier: Bytes, ancillaryData: Bytes): ByteArray {
   let packed = requester.concat(identifier).concat(ancillaryData);
   return crypto.keccak256(packed);
+}
+
+export function createCustomBondId(requester: Bytes, identifier: Bytes, ancillaryData: Bytes, currency: Bytes): string {
+  let packed = requester.concat(identifier).concat(ancillaryData).concat(currency);
+  return crypto.keccak256(packed).toHexString();
+}
+
+export function createCustomBondIdFromEvent(event: CustomBondSet): string {
+  return createCustomBondId(
+    event.params.requester,
+    event.params.identifier,
+    event.params.ancillaryData,
+    event.params.currency
+  );
 }
